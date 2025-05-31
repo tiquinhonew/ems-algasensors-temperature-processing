ad3f7aef4a238a179c3ce31c41b7094419dd3ca7
Douglas Moraes <douglas.am@gmail.com>
2025-05-30 23:42:54 -0300

    New project files

---
+++ src/test/java/com/dam/algasensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.algasensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
